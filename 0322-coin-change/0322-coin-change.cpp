86a1807e89bd0cf14cbe6fa3d6b30e645e0cdc8b
sandeepmalav0 <sandeepmalav1306@gmail.com>
2026-09-25 22:32:14 +0530

    Time: 47 ms (34.52%), Space: 48.9 MB (40.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(vector<int>& arr,int idx,int tar,vector<vector<int>>& dp){
-        if (tar == 0) return 0;
-        if(idx==0){
-            if(tar%arr[0]==0)return tar/arr[0];
-            return 1e9;
-        }
-        if(dp[idx][tar] != -1)return dp[idx][tar];
-        int notake = solve(arr,idx-1,tar,dp);
-        int take=1e9;
-        if(tar >= arr[idx]){
-            take = 1 + solve(arr,idx,tar-arr[idx],dp);
-        }
-        return dp[idx][tar]=min(notake,take);
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
-        // Using memoization DP
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1, -1));
-        int ans = solve(coins,n-1,amount,dp);
-        if(ans >= 1e9)return -1;
-        return ans;
+
+        // Using Tabulation DP
+        vector<vector<int>> dp(n,vector<int>(amount+1, 0));
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0]==0)dp[0][t]=t/coins[0];
+            else dp[0][t]=1e9;
+        }
+        for(int idx=1;idx<n;idx++){
+            for(int tar=0;tar<=amount;tar++){
+                int notake=dp[idx-1][tar];
+                int take = 1e9;
+                if(tar >= coins[idx]){
+                    take = 1 + dp[idx][tar-coins[idx]];
+                }
+                dp[idx][tar] = min(notake,take);
+            }
+        }
+        return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
     }
 };
